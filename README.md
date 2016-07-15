b906e17915e1e552780f5093d759007711e23f05
Simon Hickinbotham <sjh518@york.ac.uk>
2016-07-15 10:42:56 +0100

    Added first files

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# opencvinstall
-Notes on installing opencv and debugging with eclipse in ubuntu
+##Installing openCV
+
+Unzip the download into a directory of your choice. Then go into the ```release```  folder to do the compiling. 
+
+We want to install openCV in our user space (not the system space), so we don't need su rights to do so. This can be achieved using
+
+    cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/home/sjh/software/opencv-2.4.13/lib ..
+
+then you can do 
+
+    ./configure
+    make 
+    make install
+   
+NB actually, if you do this from the folder /home/sjh/software/opencv-2.4.13/release, you can use this without doing the make install. 
+
+##Testing a complie outside eclipse
+
+Following http://docs.opencv.org/2.4/doc/tutorials/introduction/linux_gcc_cmake/linux_gcc_cmake.html
+
+We need to tell cmake where to look for the opencv libraries, because they aren't in the usual place. This can be done by setting an environment variable like this: 
+
+    export CMAKE_PREFIX_PATH=/home/sjh/software/opencv-2.4.13/release/
+    
+NB: you'll need to add this to your .profile if you want this environment variable to be permanent.     
+
+then it's just 
+
+    cmake .
+    make
+  
+works fine
+
+
+##Creating an eclipse CDT project
+
+Following https://cmake.org/Wiki/CMake:Eclipse_UNIX_Tutorial option 1
+
+
+
---
+++ opencv_eclipse/bin/CMakeFiles/DisplayImage.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sjh/tmp/opencv_eclipse/src/DisplayImage.cpp" "/home/sjh/tmp/opencv_eclipse/bin/CMakeFiles/DisplayImage.dir/DisplayImage.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/sjh/software/opencv-2.4.13/release"
+  "/home/sjh/software/opencv-2.4.13/include"
+  "/home/sjh/software/opencv-2.4.13/include/opencv"
+  "/home/sjh/software/opencv-2.4.13/modules/core/include"
+  "/home/sjh/software/opencv-2.4.13/modules/flann/include"
+  "/home/sjh/software/opencv-2.4.13/modules/imgproc/include"
+  "/home/sjh/software/opencv-2.4.13/modules/highgui/include"
+  "/home/sjh/software/opencv-2.4.13/modules/features2d/include"
+  "/home/sjh/software/opencv-2.4.13/modules/calib3d/include"
+  "/home/sjh/software/opencv-2.4.13/modules/ml/include"
+  "/home/sjh/software/opencv-2.4.13/modules/video/include"
+  "/home/sjh/software/opencv-2.4.13/modules/legacy/include"
+  "/home/sjh/software/opencv-2.4.13/modules/objdetect/include"
+  "/home/sjh/software/opencv-2.4.13/modules/photo/include"
+  "/home/sjh/software/opencv-2.4.13/modules/gpu/include"
+  "/home/sjh/software/opencv-2.4.13/modules/ocl/include"
+  "/home/sjh/software/opencv-2.4.13/modules/nonfree/include"
+  "/home/sjh/software/opencv-2.4.13/modules/contrib/include"
+  "/home/sjh/software/opencv-2.4.13/modules/stitching/include"
+  "/home/sjh/software/opencv-2.4.13/modules/superres/include"
+  "/home/sjh/software/opencv-2.4.13/modules/ts/include"
+  "/home/sjh/software/opencv-2.4.13/modules/videostab/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
